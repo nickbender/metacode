4ead3baa6362421a63c15edc0fe7dea53fd125a9
nickbender <nickbender@gmail.com>
2014-09-16 19:06:21 -0600

    Change to php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Metacode: Learn how to learn to code.</title>
+    <link rel="stylesheet" type="text/css" href="css/site.css">
+  </head>
+  <body>
+    <header>
+      <div data-component="navbar" class="clearfix bg-mid-gray">
+      <div class="container">
+        <div class="left">
+          <div class="medium nav mobile-nav-table nav-big nav-dark">
+            <a href="#!">Home</a>
+            <a href="#!">Categories</a>
+          </div>
+        </div>
+        <div class="right">
+          <div class="medium nav nav-big nav-dark">
+            <a href="#!">Sign In</a>
+          </div>
+        </div>
+      </div>
+      </div>
+    </header>
+    <div class="container">
+      <div class="row">
+        <div class="col-center col-6">
+          <h1 class="h1 center">Metacode</h1>
+          <img class="absolute-center" src="http://i.giphy.com/x3TGbdF6Z8tsQ.gif" alt='NERD!'>
+        </div>
+      </div>  
+    </div>
+  </body>
+</html>
+
